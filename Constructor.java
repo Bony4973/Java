978712457d0b8dc716b4a2b9b27be3f3901a11db
Bony4973 <123311006+Bony4973@users.noreply.github.com>
2023-02-18 22:24:42 +0600

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,21 @@
+//CONSTRUCTOR
+public class Constructor {
+    String Name;
+    int Id;
+    int Age;
+    public Constructor(String a,int b,int c){
+        this.Name=a;
+        this.Id=b;
+        this.Age=c;
+
+    }
+    void print(){
+        System.out.println("EMployee name :" +Name);
+        System.out.println("EMployee id :" +Id);
+        System.out.println("EMployee age :" +Age);
+    }
+    public static void main(String[] args) {
+        Constructor emp= new Constructor("Bony",4973, 22);
+        emp.print();
+    }
+}
